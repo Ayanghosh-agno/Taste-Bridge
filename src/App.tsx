55d6f88e2e0b34ad29c98f5a0d0a00be986a7a45
Ayan Ghosh <70548601+Ayanghosh-agno@users.noreply.github.com>
2025-07-20 21:03:15 +0530

    Fix Leaflet icon options initialization

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,12 @@ function App() {
   useEffect(() => {
     // Fix for default markers in react-leaflet
     delete (L.Icon.Default.prototype as any)._getIconUrl;
+    
+    // Ensure options object exists before merging
+    if (!L.Icon.Default.prototype.options) {
+      L.Icon.Default.prototype.options = {};
+    }
+    
     L.Icon.Default.mergeOptions({
       iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
       iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
